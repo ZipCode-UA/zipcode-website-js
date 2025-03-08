c58b6de9515a727b0bd2d23d858b8a7d202073a4
Antonio <antoniobradach@gmail.com>
2025-03-07 23:13:39 -0500

    Add navbar

--- zipcode-website/src/app/page.js
+++ zipcode-website/src/app/page.js
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import Navbar from "./navbar";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1>Welcome to zipcode</h1>
-    </div>
+    <>
+      <Navbar />
+      
+      <div className="flex justify-center items-center h-screen">
+        <h1>Welcome to zipcode</h1>
+      </div>
+    </>
   )
 }
