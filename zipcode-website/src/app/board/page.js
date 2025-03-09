847ccca03c4c101174e351caad59cf59d571ec30
Antonio <antoniobradach@gmail.com>
2025-03-08 19:04:40 -0500

    Add event page routing

--- zipcode-website/src/app/board/page.js
+++ zipcode-website/src/app/board/page.js
@@ -1,12 +1,12 @@
 import Navbar from '../navbar';
 
 export default function Board() {
-    return (
-      <>
-        <Navbar />
-        <div className="flex justify-center items-center h-screen">
-            <h1>board page</h1>
-        </div>
-      </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen">
+        <h1>board page</h1>
+      </div>
+    </>
+  );
 }
