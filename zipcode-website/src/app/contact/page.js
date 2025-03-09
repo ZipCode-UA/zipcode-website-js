81e9ba86b09eb6b03ccd8b709b7c8c2a050cb5be
Antonio <antoniobradach@gmail.com>
2025-03-08 19:09:09 -0500

    Add page navigations

---
+++ zipcode-website/src/app/contact/page.js
@@ -0,0 +1,12 @@
+import Navbar from '../navbar';
+
+export default function Contact() {
+  return (
+    <>
+      <Navbar />
+      <div className='flex justify-center items-center h-screen'>
+        <h1> contact page</h1>
+      </div>
+    </>
+  );
+}
---
+++ zipcode-website/src/app/events/page.js
@@ -0,0 +1,12 @@
+import Navbar from '../navbar';
+
+export default function Events() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex justify-center items-center h-screen">
+        <h1> events page </h1>
+      </div>
+    </>
+)
+}
